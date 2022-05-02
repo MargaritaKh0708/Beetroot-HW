8e45c174fb8fed9e42536a1a04f4fd753e50ef33
margarita <margaritakh0708@gmail.com>
2022-05-02 18:06:32 +0300

    change links

---
+++ lesson_JS_5/index.js
@@ -0,0 +1,27 @@
+class Marker {
+    constructor(color, amountOfInk) {
+        this.color = color;
+        this.amountOfInk = amountOfInk;
+    }
+
+    // setUserText() {
+    //     let userChoise = prompt('Яким кольором маркера будеш писати (обери з тих, що вже ввів. \nАбо стандартні red чи green?');
+    //     alert(`Твій колір: ${this.color = userChoise}. Mаркеp заповнено чорнилами на ${this.amountOfInk} %`);
+    //     let userText = prompt('Введіть фразу, яку хочете написати?');
+    //     for (let i = 0; i <= (this.amountOfInk / (userText.lenght * 0.5)); i++) {
+    //         return console.log(userText);
+
+
+    //     }
+    // calcIter() {
+    //     let userText = prompt('Введіть фразу, яку хочете написати?');
+    //     return this.amountOfInk / (userText.lenght * 0.5);
+    // }
+}
+// } 
+let Marker1 = new Marker('red', 50);
+
+console.log(Marker1);
+
+// Marker1.setUserText();
+
